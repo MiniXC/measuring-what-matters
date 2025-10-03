a713fe05b71bf949a536d0415a7c671a2bdcf31a
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2025-10-03 11:01:33 +0200

    Update HuggingFaceSection.svelte

--- src/routes/HuggingFaceSection.svelte
+++ src/routes/HuggingFaceSection.svelte
@@ -18,51 +18,7 @@
             <div class="column is-10">
                 <div class="card">
                     <div class="card-content">
-                        {#if !isLoaded}
-                            <div class="has-text-centered">
-                                <div class="notification is-info">
-                                    <h3 class="title is-4">
-                                        🚀 HuggingFace Space Placeholder
-                                    </h3>
-                                    <p>
-                                        This section is reserved for embedding a
-                                        HuggingFace Space.
-                                    </p>
-                                    <p>
-                                        Click the button below to simulate
-                                        loading the space.
-                                    </p>
-                                    <button
-                                        class="button is-primary"
-                                        on:click={loadHuggingFaceSpace}
-                                    >
-                                        ✨ Load Demo Space
-                                    </button>
-                                </div>
-                            </div>
-                        {:else}
-                            <div class="has-text-centered">
-                                <div class="notification is-success">
-                                    <h3 class="title is-4">
-                                        🎉 Demo Space Loaded
-                                    </h3>
-                                    <p>
-                                        This is where your HuggingFace Space
-                                        would be embedded.
-                                    </p>
-                                    <p>
-                                        You can replace this placeholder with an
-                                        actual iframe or widget integration.
-                                    </p>
-                                    <div class="box has-background-light">
-                                        <p class="has-text-grey">
-                                            iframe
-                                            src="https://huggingface.co/spaces/your-username/your-space"
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        {/if}
+                        <iframe src="https://huggingface.co/spaces/cdminix/paper-eval-checker">
                     </div>
                 </div>
             </div>
